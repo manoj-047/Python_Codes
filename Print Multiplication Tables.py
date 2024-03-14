35efe17a04e15b98863572a58e997ea66506547e
manoj-047 <145190661+manoj-047@users.noreply.github.com>
2024-03-14 11:43:03 +0530

    Multiplication Tables.py

--- Print Multiplication Tables.py
+++ Print Multiplication Tables.py
@@ -6,6 +6,7 @@
 
 s = int(input('Enter Starting Table: '))
 e = int(input('Enter Ending Table: '))
-for j in range(s, e):
+for j in range(s, e+1):
+    print(f"\nMultiplication Table Of {j}")
     for i in range(1, 11):
-        print(j, '*', i, '=', s*i)
+        print(j, '*', i, '=', j * i)
